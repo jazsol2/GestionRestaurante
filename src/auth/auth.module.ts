999bafd2912dea4f767ac5cb045d8ae80008a00e
JazminS <jazminsolis2@gmail.com>
2025-07-18 17:28:16 -0500

    se aut para la autentificacicion y login y se a;adio cors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,15 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma/prisma.service';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: 'gestion12', 
-      signOptions: { expiresIn: '1d' },
-    }),
-  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
